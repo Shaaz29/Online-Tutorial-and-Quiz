5371a185403bcf082c981c7b13a5ded6c9dc0085
Shaaz29 <ahmadshaaz2910@gmail.com>
2025-10-25 11:39:59 +0530

    Update apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,4 +4,5 @@ class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
     
-# Ahmad Shaaz Project on Quiz
+# Ahmad Shaaz Project
+# Quiz Application
